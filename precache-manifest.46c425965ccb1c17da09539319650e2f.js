f191fb8cde792fd719c2f9b00332825762e46c03
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-20 10:55:54 +0000

    Deploy YutHelloWorld/personal-income-tax-calculator to github.com/YutHelloWorld/personal-income-tax-calculator.git:gh-pages

---
+++ precache-manifest.46c425965ccb1c17da09539319650e2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c085e5d709abed06634d",
+    "url": "/personal-income-tax-calculator/static/js/runtime~main.c085e5d7.js"
+  },
+  {
+    "revision": "9babe6536b65cc9980ff",
+    "url": "/personal-income-tax-calculator/static/js/main.9babe653.chunk.js"
+  },
+  {
+    "revision": "1c3c1247896d461fc580",
+    "url": "/personal-income-tax-calculator/static/js/4.1c3c1247.chunk.js"
+  },
+  {
+    "revision": "fae7a454fbcdd7331436",
+    "url": "/personal-income-tax-calculator/static/js/3.fae7a454.chunk.js"
+  },
+  {
+    "revision": "c2e398bf7fb0b78ec840",
+    "url": "/personal-income-tax-calculator/static/js/2.c2e398bf.chunk.js"
+  },
+  {
+    "revision": "e1b54d38a5bdc63572016ee13acef0f4",
+    "url": "/personal-income-tax-calculator/index.html"
+  }
+];
